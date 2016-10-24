480a973990a2f2b02522bcc07309ed1e9032a036
Thomas Guillerme <guillert@tcd.ie>
2016-10-24 15:49:41 +0100

    character correlation metric testing (in binary)

--- Analysis/Sandbox.R
+++ Analysis/Sandbox.R
@@ -63,6 +63,9 @@ set.correlation <- function(matrix, nchar, cor.dis) {
 }
 
 
+
+
+
 char.diff <- function(X,Y, type = "Fitch") {
 
     differences <- X-Y
@@ -80,3 +83,80 @@ char.diff <- function(X,Y, type = "Fitch") {
     #     return(1 - ( abs(sum(abs(X-Y))/length(X)-0.5)/0.5 ))
     # }
 }
+
+# Testing characters correlation
+library(testthat)
+context("Measuring characters difference")
+
+A <- c(1,0,0,0,0)
+B <- c(0,1,1,1,1)
+
+#Correlation is 1
+expect_equal(abs(cor(A,B)), 1)
+#Correlation is triangular
+expect_equal(abs(cor(A,B)), abs(cor(B,A)))
+#Difference is 0
+expect_equal(char.diff(A,B), 0)
+#Difference is triangular
+expect_equal(char.diff(A,B), char.diff(B,A))
+
+C <- c(1,1,0,0,0)
+
+#Correlation is 0.6
+expect_equal(round(abs(cor(A,C)), digit = 5), 0.61237)
+#Correlation is triangular
+expect_equal(abs(cor(A,C)), abs(cor(C,A)))
+#Difference is 0.4
+expect_equal(char.diff(A,C), 0.4)
+#Difference is triangular
+expect_equal(char.diff(A,C), char.diff(C,A))
+
+D <- c(0,1,1,0,0)
+
+#Correlation is 0.6
+expect_equal(round(abs(cor(A,D)), digit = 5), 0.40825)
+#Correlation is triangular
+expect_equal(abs(cor(A,D)), abs(cor(D,A)))
+#Difference is 0.4
+expect_equal(char.diff(A,D), 0.8)
+#Difference is triangular
+expect_equal(char.diff(A,D), char.diff(D,A))
+
+E <- c(1,0,0,1,1)
+
+#Correlation is equal to D
+expect_equal(abs(cor(D,E)), 1)
+#Correlation is triangular (with D)
+expect_equal(abs(cor(A,E)), abs(cor(A,D)))
+#Difference is equal to D
+expect_equal(char.diff(D,E), 0)
+#Difference is triangular (with D)
+expect_equal(char.diff(A,E), char.diff(A,D))
+
+# Plotting the characters differences (depending on the metric)
+## Graphical parameters 
+op <- par(mfrow = c(1,2), bty = "n")
+
+character_differences <- unlist(lapply(list(B,C,D,E), char.diff, A))
+character_correlations <- abs(unlist(lapply(list(B,C,D,E), cor, A)))
+plot(character_differences, type = "l", col = "blue", ylim = c(0,1), xaxt = "n", xlab = "", ylab = "")
+axis(1, at = 1:4, labels = c("A:B", "A:C", "A:D", "A:E"))
+lines(1-character_correlations, col = "orange")
+legend("topleft", legend = c("Difference", "1-Correlation"), lty = 1, xjust = 1, yjust = 1, col = c("blue", "orange"))
+
+# Plotting the characters differences for 50 "random" characters
+library(dispRity) ; set.seed(12345)
+# Creating the characters
+tree <- rcoal(100)
+matrix <- sim.morpho(tree, 500, rates = c(rgamma, rate = 10, shape = 5), invariant = FALSE)
+matrix <- apply(matrix, 2, as.numeric)
+
+character_differences <- apply(matrix[,-1], 2, char.diff, matrix[,1])
+character_correlations <- abs(apply(matrix[,-1], 2, cor, matrix[,1]))
+
+#Sort the values
+sorting <- sort(character_differences, index.return = TRUE)[[2]]
+
+plot(character_differences[sorting], type = "l", col = "blue", ylim = c(0,1), xlab = "", ylab = "")
+lines(1-character_correlations[sorting], col = "orange")
+legend("topleft", legend = c("Difference", "1-Correlation"), lty = 1, xjust = 1, yjust = 1, col = c("blue", "orange"))
