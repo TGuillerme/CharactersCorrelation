28cb9514f0b0a245a26725a8d62abf5fe3f2a8fd
Thomas Guillerme <guillert@tcd.ie>
2016-11-01 11:18:07 +0000

    character difference function

---
+++ Functions/char.diff.R
@@ -0,0 +1,77 @@
+#' @title Plots the correlation matrix
+#'
+#' @description Plots the correlation matrix.
+#'
+#' @param X,Y Two morphological characters.
+#' @param type How to count characters difference: \code{"Fitch"} (default) or \code{"Wagner"}.
+#' 
+#' @details
+#' \code{type}: \code{"Fitch"} counts any differences as \code{1}; \code{"Wagner"} counts the actual difference in terms of distance.
+#' For example, the \code{"Fitch"} distance between two character states \code{0} and \code{2} is \code{1}; the \code{"Wagner"} distance is \code{3}.
+#' 
+#' @examples
+#' ##
+#' 
+#' @author Thomas Guillerme
+#' @export
+#' 
+
+char.diff <- function(X,Y, type = "Fitch") {
+
+    # Convert character
+    convert.character <- function(X) {
+        if(class(X) == "numeric") {
+            X <- LETTERS[X+1]
+        } else {
+            X <- as.factor(X)
+            levels(X) <- 1:length(levels(X))
+            X <- as.numeric(X)
+        }
+        return(X)
+    }
+
+    # Transform states into similar values
+    normalise.character <- function(X, states) {
+        # Convert X to character
+        if(class(X) != "numeric") {
+            X <- convert.character(X)
+        }
+        X <- as.character(X)
+        # Get the states of X
+        states_match <- sort(match(states, X))
+
+        # Replacing the original states
+        for(state in 1:length(states)) {
+            X <- gsub(X[states_match[state]], LETTERS[state], X)
+        }
+        X <- convert.character(X)
+        return(X)
+    }
+
+    # Check if characters are binary
+    states_X <- as.numeric(levels(as.factor(X)))
+    states_Y <- as.numeric(levels(as.factor(Y)))
+
+    if(length(states_X) <= 2 & length(states_Y) <= 2) {
+        # Simple binary Fitch comparison (fast)
+        differences <- ifelse(X-Y != 0, 1, 0)
+
+        # Calculate the difference
+        return(1 - ( abs(sum(abs(differences))/length(X)-0.5)/0.5 ))
+    } else {
+        # Normalise the characters
+        X <- normalise.character(X, states_X)
+        Y <- normalise.character(Y, states_Y)
+
+        # Calculate the differences
+        differences <- X-Y        
+
+        if(type == "Fitch") {
+            # Make the differences binary (i.e. if the difference is != 0, set to 1)
+            differences <- ifelse(differences != 0, 1, 0)
+        }
+
+        # Get the characters difference
+        return(1 - ( abs(sum(abs(differences))/length(X)-0.5)/0.5 ))       
+    }
+}
