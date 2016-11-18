14a74b240878180012c42798459e78705c8112a1
Thomas Guillerme <guillert@tcd.ie>
2016-11-18 14:59:29 +0000

    Pipelined analysis for empirical data

--- Analysis/Sandbox.R
+++ Analysis/Sandbox.R
@@ -2,6 +2,11 @@
 library(Claddis)
 library(dispRity)
 source("functions.R") ; load.functions(test = FALSE)
+dyn.load("../Functions/char.diff.so")
+
+## Modify the write.nexus.data function
+write.nexus.std <- write.nexus.data
+body(write.nexus.std)[[2]] <- substitute(format <- match.arg(toupper(format), c("DNA", "PROTEIN", "STANDARD")))
 
 ###############
 # Parameters for generating the matrix
@@ -14,7 +19,7 @@ source("functions.R") ; load.functions(test = FALSE)
 # The rates (log normal or gamma)
 
 ###############
-# Looking at distribution of the correlation among empirical matrices
+# Loading the matrices
 ###############
 
 matrix_path = "../Data/100_Matrices"
@@ -31,29 +36,78 @@ find.non.numerics <- function(x) {
 }
 
 ## Read the matrices
+# matrices <- list()
+# for(matrix in 1:length(files)) {
+#     cat(files[[matrix]],"\n")
+#     matrices[[matrix]] <- ReadMorphNexus(paste(matrix_path, files[matrix], sep ="/"))$matrix
+#     matrices[[matrix]][apply(matrices[[matrix]], c(1,2), find.non.numerics)] <- "?"
+# }
+
+
+###############
+# Modifying the matrices
+###############
+
+
 matrices <- list()
-for(matrix in 1:length(files)) {
+for(matrix in 1:10) {
     cat(files[[matrix]],"\n")
     matrices[[matrix]] <- ReadMorphNexus(paste(matrix_path, files[matrix], sep ="/"))$matrix
-    # Change NAs to question marks
-    # matrices[[matrix]] <- ifelse(is.na(matrices[[matrix]]), "?", matrices[[matrix]])
-    # # Change &s to question marks
-    # <- ifelse(find.non.numerics(matrices[[matrix]]), "?", matrices[[matrix]])
     matrices[[matrix]][apply(matrices[[matrix]], c(1,2), find.non.numerics)] <- "?"
 }
 
-## Calculate the differences
-differences <- lapply(matrices, cor.matrix, char.diff)
 
-## Visualise a single matrix:
-# plot.cor.matrix(differences[[1]])
+path <- "../Data/Test/"
 
-## Estimate the distributions densities
-densities <- lapply(differences, density, na.rm = TRUE)
+system(paste("cp ../Functions/set.runs.sh ", path))
+system(paste("cp ../Functions/con.tree.sh ", path))
 
-## Plot the the densities distribution
-plot(1,1, col = white, xlim = c(0,1), ylim = c(0,1), main = "Observed characters differences in 100 matrices", xlab = "Character differences", ylab = "Density")
-lapply(densities, lines, col = "grey")
-lines(density(na.omit(unlist(differences))), col = "black")
-legend("topleft", legend = c("Cumulative", "Individual"), lty = 1, col = c("black", "grey"))
 
+for(run in 1:10) {
+
+    matrices_modified <- list()
+    character_differences <- char.diff(matrices[[run]])
+    matrices_modified$norm <- matrices[[run]]
+    matrices_modified$maxi <- modify.matrix(matrices[[run]], type = "maximise", threshold = 0.25, character_differences)
+    matrices_modified$mini <- modify.matrix(matrices[[run]], type = "minimise", threshold = 0.75, character_differences)
+
+    ###############
+    # Modifying the matrices
+    ###############
+    chain_name <- strsplit(files[[run]], ".nex")[[1]]
+
+    write.nexus.std(matrices_modified$norm, file = paste(path, chain_name, "_norm.nex", sep = ""), format = "standard")
+    write.nexus.std(matrices_modified$maxi, file = paste(path, chain_name, "_maxi.nex", sep = ""), format = "standard")
+    write.nexus.std(matrices_modified$mini, file = paste(path, chain_name, "_mini.nex", sep = ""), format = "standard")
+
+    ## Generate the commands
+    current_dir <- getwd()
+    setwd(path)
+    system(paste("sh set.runs.sh ", chain_name, " MrBayes 8", sep = ""))
+    ## Run MrBayes
+    system(paste("mb ", chain_name, "_norm.cmd", sep = ""))
+    system(paste("mb ", chain_name, "_maxi.cmd", sep = ""))
+    system(paste("mb ", chain_name, "_mini.cmd", sep = ""))
+
+    ## Compile the consensus trees
+    system(paste("sh con.tree.sh ", chain_name, sep = ""))
+
+    setwd(current_dir)
+
+    trees <- list()
+    trees$norm <- read.nexus(paste(path, chain_name, "_norm.con.tre", sep = ""))
+    trees$maxi <- read.nexus(paste(path, chain_name, "_maxi.con.tre", sep = ""))
+    trees$mini <- read.nexus(paste(path, chain_name, "_mini.con.tre", sep = ""))
+
+    ## Plotting the pdf results
+    pdf(paste(chain_name, "pdf", sep = "."), 15.22222, 8.11811)
+    nf<-layout(matrix(c(1,2,3,4,5,6),2,3,byrow = TRUE), c(2,2,2), c(3,2), FALSE)
+    #layout.show(nf)
+    plot(trees$norm, main = paste(chain_name, "-norm", sep = ""), cex = 0.6)
+    plot(trees$maxi, main = paste(chain_name, "-maxi", sep = ""), cex = 0.6)
+    plot(trees$mini, main = paste(chain_name, "-mini", sep = ""), cex = 0.6)
+    plot.char.diff.density(matrices_modified$norm, main = "")
+    plot.char.diff.density(matrices_modified$maxi, main = "")
+    plot.char.diff.density(matrices_modified$mini, main = "")
+    dev.off()
+}
