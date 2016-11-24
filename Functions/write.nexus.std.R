89f5d2d84ec74519092cc0e3a7d6aec7bc808381
Thomas Guillerme <guillert@tcd.ie>
2016-11-24 11:11:09 +0000

    write.nexus.std function modifications

--- Functions/write.nexus.std.R
+++ Functions/write.nexus.std.R
@@ -1,4 +1,5 @@
-## Copy the normal write nexus data function
-write.nexus.std <- write.nexus.data
-## Adds the STANDARD data option
+## Modified write.nexus.data function to cope with standard data
+write.nexus.std <- ape::write.nexus.data
 body(write.nexus.std)[[2]] <- substitute(format <- match.arg(toupper(format), c("DNA", "PROTEIN", "STANDARD")))
+body(write.nexus.std)[[26]][[3]][[4]] <- substitute(fcat(indent, "FORMAT", " ", DATATYPE, " ", MISSING, " ", GAP, 
+    " ", INTERLEAVE, " symbols=\"0123456789\";\n"))
