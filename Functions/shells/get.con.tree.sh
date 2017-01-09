2e28b50468a82a4b6750f07f6a860e9c0dbd541c
Thomas Guillerme <guillert@tcd.ie>
2017-01-09 12:09:17 +0000

    Get consensus trees from absolute path

---
+++ Functions/shells/get.con.tree.sh
@@ -0,0 +1,17 @@
+## Script for getting the consensus trees from the archives
+PATH_OUT="/Users/TGuillerme/Projects/CharactersCorrelation/Data/Consensus_trees/"
+
+for folder in /Volumes/LACIE\ SHARE/CharacterCorrelationData_tmp/Done/*
+do
+    ## Get the chain name
+    chain_name=$(echo ${folder} | sed 's/\/Volumes\/LACIE\ SHARE\/CharacterCorrelationData_tmp\/Done\///g' | sed 's/_[0-9][0-9][0-9][0-9][0-9][0-9][0-9].cx1b//g')
+    
+    echo ${chain_name}
+    echo "Copying trees:"
+    cp ${folder}/${chain_name}_norm.con.tre ${PATH_OUT} ; printf "."
+    cp ${folder}/${chain_name}_maxi.con.tre ${PATH_OUT} ; printf "."
+    cp ${folder}/${chain_name}_mini.con.tre ${PATH_OUT} ; printf "."
+    cp ${folder}/${chain_name}_rand.con.tre ${PATH_OUT} ; printf "."
+    echo "Done"
+
+done
