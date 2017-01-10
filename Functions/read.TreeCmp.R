eebf654e3a1623dbafa783eb395c302d40375ba1
Thomas Guillerme <guillert@tcd.ie>
2017-01-10 16:42:28 +0000

    read TreeCmp files

---
+++ Functions/read.TreeCmp.R
@@ -0,0 +1,55 @@
+#' @title Read in the TreeCmp results
+#'
+#' @description Read in as a list the results from the different tree comparisons
+#'
+#' @param chain the general name of the chain (e.g. 25t_100c)
+#'
+#' @return
+#' A list of metrics for the comparisons.
+#' 
+#' @examples
+#' ##
+#' 
+#' @author Thomas Guillerme
+#' @export
+#' 
+
+read.TreeCmp <- function(chain) {
+    ## Sanitizing
+    if(class(chain) !='character') {
+        stop('No files has been found within the given chain name.')
+    }
+
+    ## Get the different files within the path
+    files <- list.files(paste(chain, "_cmp/", sep =""))
+    if(length(files) == 0) {
+        stop('No files has been found within the given chain name.')
+    }
+
+    ## Load all the files
+    matrices <- list()
+    message("Reading the TreeCmp results:", appendLF = FALSE)
+    for(file in 1:length(files)) {
+        matrices[[file]] <- as.data.frame(read.table(paste(chain, "_cmp/",files[[file]], sep = ""), header = TRUE, row.names = 1))
+        message(".", appendLF = FALSE)
+    }
+    message("Done.\n", appendLF = FALSE)
+
+    ## Set up the list for the combined results
+    message("Combining the TreeCmp results:", appendLF = FALSE)
+    file = 1
+    maxi <- as.matrix(lapply(as.list(matrices[[file]]), `[[`, 1))
+    mini <- as.matrix(lapply(as.list(matrices[[file]]), `[[`, 2))
+    rand <- as.matrix(lapply(as.list(matrices[[file]]), `[[`, 3))
+    message(".", appendLF = FALSE)
+
+    for(file in 2:length(matrices)) {
+        maxi <- cbind(maxi, as.matrix(lapply(as.list(matrices[[file]]), `[[`, 1)))
+        mini <- cbind(mini, as.matrix(lapply(as.list(matrices[[file]]), `[[`, 2)))
+        rand <- cbind(rand, as.matrix(lapply(as.list(matrices[[file]]), `[[`, 3)))
+        message(".", appendLF = FALSE)
+    }
+    message("Done.\n", appendLF = FALSE)
+
+    return(list("maxi" = as.list(t(maxi)), "mini" = as.list(t(mini)), "rand" = as.list(t(rand))))
+}
