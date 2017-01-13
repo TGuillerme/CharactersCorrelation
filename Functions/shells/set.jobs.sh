585d8b8a7ef2bda04239be24dc3646dac894a3fc
Thomas Guillerme <guillert@tcd.ie>
2017-01-13 12:03:46 +0000

    Added PAUP home jobs

--- Functions/shells/set.jobs.sh
+++ Functions/shells/set.jobs.sh
@@ -96,35 +96,34 @@ then
     ## Run the first chain
     sh set.runs.sh ${chain1} PAUP ${CPU} outgroup
 
-    ## Run the second chain
-    if [ -n "$chain2" ]
-    then
-        sh set.runs.sh ${chain2} PAUP ${CPU} outgroup
-    fi
+    echo "paup ${chain1}_norm.ppcmd" > ${chain1}.paupjob
+    echo "paup ${chain1}_maxi.ppcmd" >> ${chain1}.paupjob
+    echo "paup ${chain1}_mini.ppcmd" >> ${chain1}.paupjob
+    echo "paup ${chain1}_rand.ppcmd" >> ${chain1}.paupjob
     
-    echo "" > ${chain1}.paupjob
-    echo "#!/bin/sh" >> ${chain1}.paupjob
-    echo "#PBS -l walltime=72:00:00" >> ${chain1}.mbjob
-    echo "#PBS -l select=${nodes}:ncpus=${CPU}:mem=2gb" >> ${chain1}.mbjob
-    echo "" >> ${chain1}.paupjob
+    # echo "" > ${chain1}.paupjob
+    # echo "#!/bin/sh" >> ${chain1}.paupjob
+    # echo "#PBS -l walltime=${job_time}:00:00" >> ${chain1}.mbjob
+    # echo "#PBS -l select=${nodes}:ncpus=${CPU}:mem=2gb" >> ${chain1}.mbjob
+    # echo "" >> ${chain1}.paupjob
 
-    echo "## Load PAUP" >> ${chain1}.paupjob
-    echo "module load mpi" >> ${chain1}.paupjob
-    echo "module load beagle-lib" >> ${chain1}.paupjob
-    echo "module load mrbayes/3.2.6" >> ${chain1}.paupjob
+    # echo "## Load PAUP" >> ${chain1}.paupjob
+    # echo "module load mpi" >> ${chain1}.paupjob
+    # echo "module load beagle-lib" >> ${chain1}.paupjob
+    # echo "module load mrbayes/3.2.6" >> ${chain1}.paupjob
 
 
-    echo "" >> ${chain1}.paupjob
-    echo "## Run the chains" >> ${chain1}.paupjob
-    echo "mpiexec mb \$HOME/CharSim/parsimony/${chain1}_norm.paupcmd" >> ${chain1}.paupjob
-    echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_maxi.paupcmd" >> ${chain1}.paupjob
-    echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_mini.paupcmd" >> ${chain1}.paupjob
-    echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_rand.paupcmd" >> ${chain1}.paupjob
-    if [ -n "$chain2" ]
-    then
-        echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_norm.paupcmd" >> ${chain1}.paupjob
-        echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_maxi.paupcmd" >> ${chain1}.paupjob
-        echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_mini.paupcmd" >> ${chain1}.paupjob
-        echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_rand.paupcmd" >> ${chain1}.paupjob
-    fi
+    # echo "" >> ${chain1}.paupjob
+    # echo "## Run the chains" >> ${chain1}.paupjob
+    # echo "mpiexec mb \$HOME/CharSim/parsimony/${chain1}_norm.paupcmd" >> ${chain1}.paupjob
+    # echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_maxi.paupcmd" >> ${chain1}.paupjob
+    # echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_mini.paupcmd" >> ${chain1}.paupjob
+    # echo "mpiexec mb \$HOME/CharSim/parsimony${chain1}_rand.paupcmd" >> ${chain1}.paupjob
+    # if [ -n "$chain2" ]
+    # then
+    #     echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_norm.paupcmd" >> ${chain1}.paupjob
+    #     echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_maxi.paupcmd" >> ${chain1}.paupjob
+    #     echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_mini.paupcmd" >> ${chain1}.paupjob
+    #     echo "mpiexec mb \$HOME/CharSim/parsimony${chain2}_rand.paupcmd" >> ${chain1}.paupjob
+    # fi
 fi
