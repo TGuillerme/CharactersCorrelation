4128df04adc3418da951ac8469d08f1adfa66493
Thomas Guillerme <guillert@tcd.ie>
2017-02-27 14:40:50 +0000

    Updated TreeCmp shell (now intakes specific trees)

--- Functions/shells/CC_TreeCmp.sh
+++ Functions/shells/CC_TreeCmp.sh
@@ -4,6 +4,7 @@
 #SYNTAX: TreeCmp -c <chain> -o <output> -f <treeformat> -d <draws> -r <rooted>
 #with:
 #-c <chain> the name of the chain
+#-r <ref> the reference tree number (1 = normal, 2 = maximum, 3 = minimum, 4 = null)
 #-o <output> OPTIONAL the output name (if missing, is set to <chain>)
 #-f <treeformat> OPTIONAL the tree in and out format (if missing, is set to '.nex')
 #-d <draws> OPTIONAL the number of draws for the comparison (if missing, is set to '1000')
@@ -28,6 +29,10 @@ key="$1"
 case $key in
     -c|--chain)
         CHAIN="$2"
+        shift
+        ;;
+    -r|--ref)
+        REF="$2"
         shift
         ;;
     -o|--output)
@@ -57,6 +62,11 @@ if [ "$CHAIN" == "" ]
 then
     echo "ERROR: --chain is missing!"
     exit
+fi
+
+if [ "$REF" == "" ]
+then
+    REF=1
 fi
 
 if [ "$OUTPUT" == "" ]
@@ -97,6 +107,7 @@ else
     fi
 fi
 
+# ##DEBUG
 # echo "chain name = ${CHAIN}"
 # echo "output name = ${OUTPUT}"
 # echo "format out = ${FORMAT}"
@@ -116,7 +127,23 @@ else
 fi
 
 ## Get the Chain folder name
-chain_folder=$(echo $CHAIN | sed 's/c_.*/c_cmp/g')
+if [ "$REF" == 1 ]
+then
+    chain_folder=$(echo $CHAIN | sed 's/c_.*/c_cmp_norm/g')
+fi
+if [ "$REF" == 2 ]
+then
+    chain_folder=$(echo $CHAIN | sed 's/c_.*/c_cmp_maxi/g')
+fi
+if [ "$REF" == 3 ]
+then
+    chain_folder=$(echo $CHAIN | sed 's/c_.*/c_cmp_mini/g')
+fi
+if [ "$REF" == 4 ]
+then
+    chain_folder=$(echo $CHAIN | sed 's/c_.*/c_cmp_null/g')
+fi
+
 
 ## Create the folder if needed
 if ls -d | grep ${chain_folder} > /dev/null
@@ -136,8 +163,26 @@ then
         sh Nex2New.sh ${CHAIN} "newick"
 
         ## Save the three trees in a same file
-        cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_ref.tre
-        cat ${CHAIN}_maxi.con.tre.newick > ${CHAIN}_comb.con.tre
+        
+        if [ "$REF" == 1 ]
+        then
+            cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 2 ]
+        then
+            cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 3 ]
+        then
+            cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 4 ]
+        then
+            cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_ref.tre
+        fi
+
+        cat ${CHAIN}_norm.con.tre.newick > ${CHAIN}_comb.con.tre
+        cat ${CHAIN}_maxi.con.tre.newick >> ${CHAIN}_comb.con.tre
         cat ${CHAIN}_mini.con.tre.newick >> ${CHAIN}_comb.con.tre
         cat ${CHAIN}_rand.con.tre.newick >> ${CHAIN}_comb.con.tre
         rm ${CHAIN}_*.newick
@@ -145,56 +190,33 @@ then
         sed -i -e 's/\[\&U\]/'"${root}"'/g' ${CHAIN}_comb.con.tre ; rm ${CHAIN}_comb.con.tre-e
     else
         ## Save the three trees in a same file
-        cat ${CHAIN}_norm.con.tre > ${CHAIN}_ref.tre
-        cat ${CHAIN}_maxi.con.tre > ${CHAIN}_comb.con.tre
+        
+        if [ "$REF" == 1 ]
+        then
+            cat ${CHAIN}_norm.con.tre > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 2 ]
+        then
+            cat ${CHAIN}_norm.con.tre > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 3 ]
+        then
+            cat ${CHAIN}_norm.con.tre > ${CHAIN}_ref.tre
+        fi
+        if [ "$REF" == 4 ]
+        then
+            cat ${CHAIN}_norm.con.tre > ${CHAIN}_ref.tre
+        fi
+        
+        cat ${CHAIN}_norm.con.tre > ${CHAIN}_comb.con.tre
+        cat ${CHAIN}_maxi.con.tre >> ${CHAIN}_comb.con.tre
         cat ${CHAIN}_mini.con.tre >> ${CHAIN}_comb.con.tre
         cat ${CHAIN}_rand.con.tre >> ${CHAIN}_comb.con.tre
         ## Change the eventual root
         sed -i -e 's/\[\&U\]/'"${root}"'/g' ${CHAIN}_comb.con.tre ; rm ${CHAIN}_comb.con.tre-e
     fi
 
-# else
-#     ## Prepare the comparison files (draws)
-
 fi
-
-#     #Is the input tree a chain?
-#     if grep '1 file(s)' TreeCmp_settings.tmp >/dev/null
-#     then
-#         echo $INPtree > ${output}_tmp/treenames.list
-
-#         #Counting the number of trees available
-#         if grep 'newick trees' TreeCmp_settings.tmp > /dev/null
-#         then
-#             #Number of newick trees per files
-#             REFntrees=$(cat $REFtree | wc -l)
-#             INPntrees=$(cat $INPtree | wc -l)
-#         else
-#             #Number of nexus trees per files
-#             REFntrees=$(grep "[[:space:]]TREE[[:space:]]\|[[:space:]]Tree[[:space:]]\|[[:space:]]tree[[:space:]]" $REFtree | wc -l)
-#             INPntrees=$(grep "[[:space:]]TREE[[:space:]]\|[[:space:]]Tree[[:space:]]\|[[:space:]]tree[[:space:]]" $INPtree | wc -l)
-#         fi
-
-#         #Creating the random draw list
-#         echo "if($REFntrees < $draws) {
-#                 REFrep=TRUE } else {
-#                 REFrep=FALSE}
-#             if($INPntrees < $draws) {
-#                 INPrep=TRUE } else {
-#                 INPrep=FALSE }
-#             #Saves the list of trees to sample in the REF and INP files    
-#             write(sample(seq(1:$REFntrees), $draws, replace=REFrep), file=\"${output}_tmp/REFtree.sample\", ncolumns=1)
-#             write(sample(seq(1:$INPntrees), $draws, replace=INPrep), file=\"${output}_tmp/INPtree.sample\", ncolumns=1) " | R --no-save >/dev/null
-       
-#     else
-#         echo "A single tree file must be given if the number of draws is > 1."
-#         echo "The single tree file can however contain multiple trees."
-#         rm TreeCmp_settings.tmp
-#         rm -R ${output}_tmp/
-#         exit
-#     fi
-
-
 
 ## RUNNING THE COMPARISONS
 if [ "$DRAWS" == 1 ]
@@ -206,116 +228,5 @@ then
     printf "."
     echo "Done."
 fi
-# else 
 
-#     ## Run the tree comparison for draw > 1
-
-# fi
-#     #More than one draw, comparison is in treeset method
-#     echo "Comparing the trees $draws times"
-#     for n in $(seq -w 1 $draws)
-#     do
-#         #Extracting the trees from the random draw list
-#         i=$(sed -n ''"${n}p"'' ${output}_tmp/REFtree.sample)
-#         j=$(sed -n ''"${n}p"'' ${output}_tmp/INPtree.sample)
-#         if grep 'newick trees' TreeCmp_settings.tmp > /dev/null
-#         then
-#             #Newick trees
-#             sed -n ''"${i}p"'' $REFtree > ${output}_tmp/REF.tre
-#             sed -n ''"${j}p"'' $INPtree > ${output}_tmp/INP.tre
-#         else
-#             #Nexus trees
-#             ENDheaderREF=$(grep -n "[[:space:]]TREE[[:space:]]\|[[:space:]]Tree[[:space:]]\|[[:space:]]tree[[:space:]]" $REFtree | cut -f1 -d: | sed -n '1p')
-#             ENDheaderINP=$(grep -n "[[:space:]]TREE[[:space:]]\|[[:space:]]Tree[[:space:]]\|[[:space:]]tree[[:space:]]" $INPtree | cut -f1 -d: | sed -n '1p')
-#             let "ENDheaderREF -= 1"
-#             let "ENDheaderINP -= 1"
-#             inexus=$i
-#             jnexus=$j
-#             let "inexus += $ENDheaderREF"
-#             let "jnexus += $ENDheaderINP"
-#             sed -n '1,'"$ENDheaderREF"'p' $REFtree > ${output}_tmp/REF.tre
-#             #echo ";" >> ${output}_tmp/REF.tre
-#             sed -n ''"${inexus}p"'' $REFtree >> ${output}_tmp/REF.tre
-#             echo "END;" >> ${output}_tmp/REF.tre
-#             sed -n '1,'"$ENDheaderINP"'p' $INPtree > ${output}_tmp/INP.tre
-#             #echo ";" >> ${output}_tmp/INP.tre
-#             sed -n ''"${jnexus}p"'' $INPtree >> ${output}_tmp/INP.tre
-#             echo "END;" >> ${output}_tmp/INP.tre
-#         fi
-
-#         #Comparing the trees
-#         java -jar TreeCmp/bin/TreeCmp.jar -r ${output}_tmp/REF.tre -d ${metrics} -i ${output}_tmp/INP.tre -o ${output}_tmp/Cmp.${n}.tmp > /dev/null
-#         printf .
-
-#     done
-#     echo "Done"
-
-#    java -jar TreeCmp/bin/TreeCmp.jar -r ${chain}_norm.con.trees -d ${metrics} -i ${chain}_norm.con.trees -o ${output}.Cmp > /dev/null
-
-# fi
-
-
-## MANAGING THE OUTPUT
-
-
-
-# #BUILDING THE OUTPUT
-# if grep '1 draw(s)' TreeCmp_settings.tmp >/dev/null
-# then
-#     echo "Renaming the output comparisons"
-#     totalTrees=$(cat ${output}_tmp/treenames.list | wc -l)
-
-#     #Testing if a Multi tree file was used
-#     MultiTreeTest=$(cat ${output}_tmp/${output}.Cmp.tmp | wc -l)
-#     let "MultiTreeTest -= 1"
-#     #Creating the header
-#     echo "Reference Tree\tTree name" > ${output}_tmp/header.tmp
-
-#     if  echo $totalTrees | grep $MultiTreeTest > /dev/null
-#     then
-#         #Creating the header using the treenames.list file
-#         for n in $(seq 1 $totalTrees)
-#         do
-#             treename=$(sed -n ''"${n}"'p' ${output}_tmp/treenames.list)
-#             echo "$REFtree\t$treename"  >> ${output}_tmp/header.tmp
-#             printf .
-#         done
-#     else
-#         #Creating a new header from scratch
-#         for n in $(seq 1 $MultiTreeTest)
-#         do
-#             treename=$(sed -n '1p' ${output}_tmp/treenames.list)
-#             echo "$REFtree\t$treename.${n}"  >> ${output}_tmp/header.tmp
-#             printf .
-#         done
-#     fi
-
-# else
-#     echo "Combining the output comparisons"
-#     sed -n '1p' $(ls ${output}_tmp/Cmp.*.tmp | sed -n '1p') > ${output}_tmp/${output}.Cmp.tmp
-
-#     #Combining the output comparisons
-#     for n in $(seq -w 1 $draws)
-#     do
-#         sed -n '2p' ${output}_tmp/Cmp.${n}.tmp >> ${output}_tmp/${output}.Cmp.tmp
-#         printf .
-#     done
-
-#     #Creating the header using the randon draws list
-#     #Creating the header
-#     echo "Ref.tree@Inp.tree" | sed 's/@/'"$(printf '\t')"'/g' > ${output}_tmp/header.tmp
-#     paste ${output}_tmp/REFtree.sample ${output}_tmp/INPtree.sample >> ${output}_tmp/header.tmp
-# fi
-
-# #Pasting the header with the .Cmp.tmp file
-# paste ${output}_tmp/header.tmp ${output}_tmp/${output}.Cmp.tmp > ${output}_tmp/${output}.Cmp.tmp2
-# echo "write.table(read.table(\"${output}_tmp/${output}.Cmp.tmp2\", header=TRUE, sep='\t')[,-3], file=\"${output}_tmp/${output}.Cmp\", sep='\t', row.names=FALSE)" | R --no-save >/dev/null
-# sed 's/"//g' ${output}_tmp/${output}.Cmp > ${output}.Cmp
-# echo "Done"
-
-# #CLEANING
-# rm TreeCmp_settings.tmp
-# rm -R ${output}_tmp/
-
-
-# #end
+done
