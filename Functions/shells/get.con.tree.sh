cb2798ea2c2eab34c4dbfc7574a24696237debe2
Thomas Guillerme <guillert@tcd.ie>
2017-05-28 16:11:29 +0100

    Function now also copies the true tree

--- Functions/shells/get.con.tree.sh
+++ Functions/shells/get.con.tree.sh
@@ -1,37 +1,27 @@
-## Script for getting the consensus trees from the archives
-PATH_OUT="/Users/TGuillerme/Projects/CharactersCorrelation/Data/Bayesian/Consensus_trees/"
+## Get the chain prefix
+chain_prefix=$1
 
-for folder in *.cx1b
+## Script for getting the consensus trees from the archives
+PATH_OUT="/Users/TGuillerme/Projects/CharactersCorrelation/Data/Trees_out/Consensus_trees/Bayesian"
+
+for folder in ${chain_prefix}*
 do
     ## Get the chain name
-    chain_name=$(echo ${folder} | sed 's/\/Volumes\/LACIE\ SHARE\/CharacterCorrelationData_tmp\/Done\///g' | sed 's/_[0-9][0-9][0-9][0-9][0-9][0-9][0-9].cx1b//g')
+    chain_name=$(echo ${folder} | sed 's/\/Volumes\/CharacterCorrelation\/CharacterCorrelationData\///g')
     
     echo ${chain_name}
     echo "Copying trees:"
-    cp ${folder}/${chain_name}_norm.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_maxi.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_mini.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_rand.con.tre ${PATH_OUT} ; printf "."
-    echo "Done"
-
-done
-
-
-
-## Script for getting the consensus trees from the archives
-PATH_OUT="/Users/TGuillerme/Projects/CharactersCorrelation/Bayesian/Data/Consensus_trees/"
-
-for folder in *.cx1b
-do
-    ## Get the chain name
-    chain_name=$(echo ${folder} | sed 's/\/Volumes\/LACIE\ SHARE\/CharacterCorrelationData_tmp\/Done\///g' | sed 's/_[0-9][0-9][0-9][0-9][0-9][0-9][0-9].cx1b//g')
+    cp ${folder}/${chain_name}_norm.con.tre ${PATH_OUT}/${chain_prefix} ; printf "."
+    cp ${folder}/${chain_name}_maxi.con.tre ${PATH_OUT}/${chain_prefix} ; printf "."
+    cp ${folder}/${chain_name}_mini.con.tre ${PATH_OUT}/${chain_prefix} ; printf "."
+    cp ${folder}/${chain_name}_rand.con.tre ${PATH_OUT}/${chain_prefix} ; printf "."
     
-    echo ${chain_name}
-    echo "Copying trees:"
-    cp ${folder}/${chain_name}_norm.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_maxi.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_mini.con.tre ${PATH_OUT} ; printf "."
-    cp ${folder}/${chain_name}_rand.con.tre ${PATH_OUT} ; printf "."
+    ## Adding the true tree as well
+    cp /Users/TGuillerme/Projects/CharactersCorrelation/Data/Simulations/Trees/${chain_name}_truetree.nex ${PATH_OUT}/${chain_prefix} ; printf "."
+    
+    ## Move the folder
+    mv ${folder} Done/
     echo "Done"
+
 
 done
