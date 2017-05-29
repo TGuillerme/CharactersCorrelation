8d5a312055b07bc141b86d6e1ad1f72003210053
Thomas Guillerme <guillert@tcd.ie>
2017-05-29 14:03:42 +0100

    Added folder filter

--- Functions/shells/get.con.tree.sh
+++ Functions/shells/get.con.tree.sh
@@ -9,6 +9,14 @@ do
     ## Get the chain name
     chain_name=$(echo ${folder} | sed 's/\/Volumes\/CharacterCorrelation\/CharacterCorrelationData\///g')
     
+    list=$(ls ${PATH_OUT})
+    if echo $list | grep -q ${chain_prefix}
+    then
+        silent="silent"
+    else
+        mkdir ${PATH_OUT}/${chain_prefix}
+    fi
+
     echo ${chain_name}
     echo "Copying trees:"
     cp ${folder}/${chain_name}_norm.con.tre ${PATH_OUT}/${chain_prefix} ; printf "."
