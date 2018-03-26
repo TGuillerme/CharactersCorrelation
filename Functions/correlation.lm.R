7787c74c44f18d4d78839baa2c49bd2ce4dd5ab9
Thomas Guillerme <guillert@tcd.ie>
2018-03-26 14:23:49 +1000

    correlation functions

---
+++ Functions/correlation.lm.R
@@ -0,0 +1,226 @@
+#' @title Get matrices CD
+#'
+#' @description Get the matrices character difference of a chain
+#'
+#' @param chain The chain name (e.g. "25t_100c")
+#' @param path The path (e.g. "../Data/Simulations/Matrices")
+#' @param verbose whether to be verbose or not.
+#' @param length.out optional, how many matrices to analyse
+#' @param cent.tend optional, whether to summarise the results directly
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+get.matrix.CD <- function(chain, path, verbose = FALSE, length.out, cent.tend = NULL) {
+    ## Get the chain names
+    chain_names <- list.files(path, pattern = chain)
+
+    ## Split per matrix type
+    matrix_types <- list("norm", "mini", "maxi", "rand")
+    split.per.type <- function(type, chain_names, length.out) {
+        if(missing(length.out)) {
+            return(as.list(chain_names[grep(type, chain_names)]))
+        } else {
+            return(as.list(chain_names[grep(type, chain_names)][1:length.out]))
+        }
+    }
+    if(length.out < length(chain_names)) {
+        chain_names_type <- lapply(matrix_types, split.per.type, chain_names, length.out)
+    } else {
+        chain_names_type <- lapply(matrix_types, split.per.type, chain_names)
+        warning(paste0("Only ", length(chain_names), " matrices loaded."))
+    }
+
+    ## Load one matrix and calculate the CD
+    get.one.CD <- function(one_chain, path, verbose = FALSE) {
+        matrix_list <- read.nexus.data(paste0(path, "/", one_chain))
+        matrix <- matrix(unlist(matrix_list), nrow = length(matrix_list), ncol = length(matrix_list[[1]]), byrow = TRUE)
+        char_diff <- char.diff(matrix)
+        if(verbose) message(".", appendLF = FALSE)
+        return(char_diff)
+    }
+
+    if(verbose) message(paste0("Getting character difference for ", chain, ":"))
+    character_differences <- lapply(chain_names_type, lapply, get.one.CD, path, verbose = verbose)
+    if(verbose) message("Done.\n", appendLF = FALSE)
+
+    ## Name the list
+    names(character_differences) <- matrix_types
+
+    ## summarises
+    if(!is.null(cent.tend)) {
+        character_differences <- lapply(character_differences, lapply, function(X, cent.tend) return(cent.tend(as.numeric(X))), cent.tend)
+    }
+
+    return(character_differences)
+}
+
+
+#' @title Get all matrices CD
+#'
+#' @description Get the matrices character difference of a chain
+#'
+#' @param path The path (e.g. "../Data/Simulations/Matrices")
+#' @param verbose whether to be verbose or not.
+#' @param length.out optional, how many matrices to analyse
+#' @param cent.tend optional, whether to summarise the results directly
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+get.all.matrix.CD <- function(path, verbose = FALSE, length.out, cent.tend = NULL) {
+
+    ## Get all the chains
+    taxa <- paste0(c(25, 75, 150), "t")
+    characters <- paste0(c(100, 350, 1000), "c")
+    all_chains <- lapply(as.list(taxa), function(taxa, characters) return(as.list(paste(taxa, characters, sep = "_"))), characters)
+
+    ## Apply get.matrix.CD on all_chains
+    all_char_diff <- lapply(all_chains, lapply, get.matrix.CD, path = path, verbose = verbose, length.out = length.out, cent.tend = cent.tend)
+    names(all_char_diff) <- taxa
+    for(tax in 1:length(all_char_diff)) {
+        names(all_char_diff[[tax]]) <- characters
+    }
+
+    return(all_char_diff)
+}
+
+#' @title Runs one linear model
+#'
+#' @description Runs one linear model
+#'
+#' @param matrix_cd the list of matrices with CD values
+#' @param whole_data the list of all the data
+#' @param chain the chain name (corresponding to the one used to obtain the matrix_cd)
+#' @param metric which metric to plot (\code{"RF"} or \code{"Triplets"})
+#' @param method which method to plot (\code{"Bayesian"} or \code{"Parsimony"}) 
+#' @param cent.tend the central tendency to plot for matrix_cd (\code{default = mean})
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+run.lm <- function(matrices_cd, whole_data, chain, metric, method, cent.tend = NULL) {
+
+    ## Get the metric
+    if(metric == "RF") {
+        metric_value <- 2
+    } else {
+        if(metric == "Triplets") {
+            metric_value <- 4
+        } else {
+            stop("metric must be \"RF\" or \"Triplets\".")
+        }
+    }
+
+    ## Get the method
+    if(method == "Bayesian") {
+        method_value <- 1
+    } else {
+        if(metric == "Parsimony") {
+            method_value <- 2
+        } else {
+            stop("metric must be \"Bayesian\" or \"Parsimony\".")
+        }
+    }
+
+    ## Get the chain values
+    chain_values <- strsplit(chain, split = "_")[[1]]
+    taxa_number <- as.numeric(strsplit(chain_values[1], split = "t")[[1]])
+    if(!taxa_number %in% c(25, 75, 150)) {
+        stop(paste0("In ", chain, " taxa number (#t) must be 25, 75 or 150."))
+    }
+    if(taxa_number == 25) {
+        taxa_value <- 1
+    } else {
+        taxa_value <- ifelse(taxa_number == 75, 2, 3)
+    }
+    character_number <- as.numeric(strsplit(chain_values[2], split = "c")[[1]])
+    if(!character_number %in% c(100, 350, 1000)) {
+        stop(paste0("In ", chain, " character number (#c) must be 100, 350 or 1000."))
+    }
+    if(character_number == 100) {
+        character_value <- 1
+    } else {
+        character_value <- ifelse(taxa_number == 350, 2, 3)
+    }
+
+    ## Length of matrices_cd
+    length.out <- length(matrices_cd[[1]])
+    length_whole_data <- length(whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value])
+    if(length.out > length_whole_data) {
+        length.out <- length_whole_data
+    }
+
+    ## Getting the data table ready
+    if(!is.null(cent.tend)) {
+        data_table <- data.frame(
+            "Metric" = c(whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value][1:length.out],
+                         whole_data[[taxa_value]][[character_value]][[method_value]]$norm$maxi[, metric_value][1:length.out],
+                         whole_data[[taxa_value]][[character_value]][[method_value]]$norm$rand[, metric_value][1:length.out]),
+                "CD" = c(unlist(lapply(matrices_cd$mini, cent.tend))[1:length.out],
+                         unlist(lapply(matrices_cd$maxi, cent.tend))[1:length.out],
+                         unlist(lapply(matrices_cd$rand, cent.tend))[1:length.out])
+        )
+    } else {
+        data_table <- data.frame(
+        "Metric" = c(whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value][1:length.out],
+                     whole_data[[taxa_value]][[character_value]][[method_value]]$norm$maxi[, metric_value][1:length.out],
+                     whole_data[[taxa_value]][[character_value]][[method_value]]$norm$rand[, metric_value][1:length.out]),
+            "CD" = c(unlist(matrices_cd$mini)[1:length.out],
+                     unlist(matrices_cd$maxi)[1:length.out],
+                     unlist(matrices_cd$rand)[1:length.out])
+        )
+    }
+
+    ## Running the model
+    return(lm(Metric ~ CD, data = data_table))
+}
+
+
+#' @title Runs all linear model
+#'
+#' @description Runs all linear model
+#'
+#' @param all_matrix_cd the list of matrices with CD values
+#' @param whole_data the list of all the data
+#' @param metric which metric to plot (\code{"RF"} or \code{"Triplets"})
+#' @param method which method to plot (\code{"Bayesian"} or \code{"Parsimony"}) 
+#' @param cent.tend optional, the central tendency to plot for matrix_cd (\code{default = mean})
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+run.all.lm <- function(all_matrices_cd, whole_data, metric, method, cent.tend = NULL) {
+
+    ## Get all the chains
+    taxa <- paste0(c(25, 75, 150), "t")
+    characters <- paste0(c(100, 350, 1000), "c")
+    all_chains <- lapply(as.list(taxa), function(taxa, characters) return(as.list(paste(taxa, characters, sep = "_"))), characters)
+
+    ## Run all the lms
+    all_models_out <- list()
+    for(tax in 1:length(all_matrices_cd)) {
+        all_models_out[[tax]] <- list()
+        for(char in 1:length(all_matrices_cd[[1]])) {
+            all_models_out[[tax]][[char]] <- run.lm(all_matrices_cd[[tax]][[char]], whole_data, chain = all_chains[[tax]][[char]], method = method, metric = metric, cent.tend = cent.tend)
+        }
+        names(all_models_out[[tax]]) <- characters
+    }
+    names(all_models_out) <- taxa
+
+    return(all_models_out)
+}
---
+++ Functions/correlation.results.R
@@ -0,0 +1,234 @@
+#' @title Plots one correlation results
+#'
+#' @description Plots one correlation results
+#'
+#' @param matrices_cd the list of matrices with CD values
+#' @param whole_data the list of all the data
+#' @param chain the chain name (corresponding to the one used to obtain the matrix_cd)
+#' @param metric which metric to plot (\code{"RF"} or \code{"Triplets"})
+#' @param method which method to plot (\code{"Bayesian"} or \code{"Parsimony"}) 
+#' @param model optional, a linear model
+#' @param col a list of colours to differentiate the scenarios
+#' @param rounding the number of digits for the plotted values
+#' @param cent.tend optional, the central tendency to plot for matrix_cd
+#' @param legend whether to plot the legend or not
+#' @param xlab the x label
+#' @param ylab the y label
+#' @param main optional, the title
+#' @param additional plot arguments
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+one.correlation.plot <- function(matrices_cd, whole_data, chain, metric, method, model, col = c("blue", "orange", "green"), rounding = 3, cent.tend = NULL, legend = FALSE, xlab = "mean CD", ylab = "Metric", main, ...) {
+    ## Get the metric
+    if(metric == "RF") {
+        metric_value <- 2
+    } else {
+        if(metric == "Triplets") {
+            metric_value <- 4
+        } else {
+            stop("metric must be \"RF\" or \"Triplets\".")
+        }
+    }
+
+    ## Get the method
+    if(method == "Bayesian") {
+        method_value <- 1
+    } else {
+        if(metric == "Parsimony") {
+            method_value <- 2
+        } else {
+            stop("metric must be \"Bayesian\" or \"Parsimony\".")
+        }
+    }
+
+    ## Summarise the model
+    if(class(model) == "lm") {
+        summary_model <- summary(model)
+    } else {
+        if(class(model) == "summary.lm") {
+            summary_model <- model
+        } else {
+            stop("model must be of class \"lm\" or \"summary.lm\"")
+        }
+    }
+
+    ## Get the chain values
+    chain_values <- strsplit(chain, split = "_")[[1]]
+    taxa_number <- as.numeric(strsplit(chain_values[1], split = "t")[[1]])
+    if(!taxa_number %in% c(25, 75, 150)) {
+        stop(paste0("In ", chain, " taxa number (#t) must be 25, 75 or 150."))
+    }
+    if(taxa_number == 25) {
+        taxa_value <- 1
+    } else {
+        taxa_value <- ifelse(taxa_number == 75, 2, 3)
+    }
+    character_number <- as.numeric(strsplit(chain_values[2], split = "c")[[1]])
+    if(!character_number %in% c(100, 350, 1000)) {
+        stop(paste0("In ", chain, " character number (#c) must be 100, 350 or 1000."))
+    }
+    if(character_number == 100) {
+        character_value <- 1
+    } else {
+        character_value <- ifelse(taxa_number == 350, 2, 3)
+    }
+
+    ## Get the default main
+    if(missing(main)) {
+        main <- paste0("Correlation between character difference and ", metric, " (", method, ")")
+    }
+
+    ## Length of matrices_cd
+    length.out <- length(matrices_cd[[1]])
+    length_whole_data <- length(whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value])
+    if(length.out > length_whole_data) {
+        length.out <- length_whole_data
+    }
+
+    plot(NULL, xlab = xlab, ylab = ylab, ylim = c(0,1), xlim = c(0,1), main = main, ...)
+    if(!is.null(cent.tend)) {
+        points(unlist(lapply(matrices_cd$mini, cent.tend))[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value][1:length.out], pch = 19, col = col[1], ...)
+        points(unlist(lapply(matrices_cd$maxi, cent.tend))[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$maxi[, metric_value][1:length.out], pch = 19, col = col[2], ...)
+        points(unlist(lapply(matrices_cd$rand, cent.tend))[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$rand[, metric_value][1:length.out], pch = 19, col = col[3], ...)
+    } else {
+        points(unlist(matrices_cd$mini)[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$mini[, metric_value][1:length.out], pch = 19, col = col[1], ...)
+        points(unlist(matrices_cd$maxi)[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$maxi[, metric_value][1:length.out], pch = 19, col = col[2], ...)
+        points(unlist(matrices_cd$rand)[1:length.out], whole_data[[taxa_value]][[character_value]][[method_value]]$norm$rand[, metric_value][1:length.out], pch = 19, col = col[3], ...)
+
+    }
+
+    ## Adding the legend
+    if(legend) {
+        legend("bottomright", pch = 19, col = col, legend = c("mini", "maxi", "rand"), ...)
+    }
+
+    ## Adding the model
+    if(!missing(model)) {
+        ## Adding the model line
+        abline(model)
+        ## Adding the model values
+        text(0, 0.2, "Metric ~ Character difference", pos = 4, ...)
+        text(0, 0.15, paste0("Intercept = ",
+                             round(summary_model$coefficients[1,1], digit = rounding),
+                             " (p = ",
+                             round(summary_model$coefficients[1,4], digit = rounding),
+                             ")"), pos = 4, ...)
+        text(0, 0.10, paste0("Slope = ",
+                             round(summary_model$coefficients[2,1], digit = rounding),
+                             " (p = ",
+                             round(summary_model$coefficients[2,4], digit = rounding),
+                             ")"), pos = 4, ...)
+        text(0, 0.05, paste0("R^2 (adj) = ",
+                             round(summary_model$adj.r.squared, digit = rounding)
+                             ), pos = 4, ...)
+    }
+}
+
+
+
+
+#' @title Plots all correlation results
+#'
+#' @description Plots all correlation results
+#'
+#' @param all_matrices_cd the list of matrices with CD values
+#' @param whole_data the list of all the data
+#' @param metric which metric to plot (\code{"RF"} or \code{"Triplets"})
+#' @param method which method to plot (\code{"Bayesian"} or \code{"Parsimony"}) 
+#' @param models optional, a linear model
+#' @param col a list of colours to differentiate the scenarios
+#' @param rounding the number of digits for the plotted values
+#' @param cent.tend the central tendency to plot for matrix_cd
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+#' 
+
+all.correlation.plot <- function(all_matrices_cd, whole_data, metric, method, models, col = c("blue", "orange", "green"), rounding = 3, cent.tend = NULL, ...) {
+    
+    # ## Chain names
+    # taxa <- paste0(c(25, 75, 150), "t")
+    # characters <- paste0(c(100, 350, 1000), "c")
+    # all_chains <- lapply(as.list(taxa), function(taxa, characters) return(as.list(paste(taxa, characters, sep = "_"))), characters)
+
+
+    par(bty = "n", mfrow = c(3, 3))
+    ## First row
+    one.correlation.plot(all_matrices_cd[[1]][[1]], whole_data, chain = all_chains[[1]][[1]], metric = metric, method = method, model = models[[1]][[1]], col = col, rounding = rounding, cent.tend = cent.tend, legend = TRUE, xlab = "", ylab = paste0("25t - ", metric), main = "", ...)
+    one.correlation.plot(all_matrices_cd[[1]][[2]], whole_data, chain = all_chains[[1]][[2]], metric = metric, method = method, model = models[[1]][[2]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "", ylab = "", main = "Bayesian", ...)
+    one.correlation.plot(all_matrices_cd[[1]][[3]], whole_data, chain = all_chains[[1]][[3]], metric = metric, method = method, model = models[[1]][[3]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "", ylab = "", main = "", ...)
+
+    ## Second row
+    one.correlation.plot(all_matrices_cd[[2]][[1]], whole_data, chain = all_chains[[2]][[1]], metric = metric, method = method, model = models[[2]][[1]], col = col, rounding = rounding, cent.tend = cent.tend, legend = TRUE, xlab = "", ylab = paste0("75t - ", metric), main = "", ...)
+    one.correlation.plot(all_matrices_cd[[2]][[2]], whole_data, chain = all_chains[[2]][[2]], metric = metric, method = method, model = models[[2]][[2]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "", ylab = "", main = "", ...)
+    one.correlation.plot(all_matrices_cd[[2]][[3]], whole_data, chain = all_chains[[2]][[3]], metric = metric, method = method, model = models[[2]][[3]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "", ylab = "", main = "", ...)
+
+    ## Third row
+    one.correlation.plot(all_matrices_cd[[3]][[1]], whole_data, chain = all_chains[[3]][[1]], metric = metric, method = method, model = models[[3]][[1]], col = col, rounding = rounding, cent.tend = cent.tend, legend = TRUE, xlab = "100c - CD", ylab = paste0("150t - ", metric), main = "", ...)
+    one.correlation.plot(all_matrices_cd[[3]][[2]], whole_data, chain = all_chains[[3]][[2]], metric = metric, method = method, model = models[[3]][[2]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "350c - CD", ylab = "", main = "", ...)
+    one.correlation.plot(all_matrices_cd[[3]][[3]], whole_data, chain = all_chains[[3]][[3]], metric = metric, method = method, model = models[[3]][[3]], col = col, rounding = rounding, cent.tend = cent.tend, legend = FALSE, xlab = "1000c - CD", ylab = "", main = "", ...)
+
+}
+
+#' @title Correlation tables
+#'
+#' @description Correlation tables
+#'
+#' @param models optional, a linear model
+#' @param metric which metric to plot (\code{"RF"} or \code{"Triplets"})
+#' @param method which method to plot (\code{"Bayesian"} or \code{"Parsimony"}) 
+#' @param rounding the number of digits for the plotted values
+#'
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+correlation.table <- function(models) {
+
+    ## Summarising all models
+    summary_models <- lapply(models, lapply, summary)
+
+    ## Organise as a table
+    intercepts <- unlist(lapply(summary_models, lapply, function(X) return(X$coefficients[1,1])))
+    p_intercepts <- unlist(lapply(summary_models, lapply, function(X) return(X$coefficients[1,4])))
+    slopes <- unlist(lapply(summary_models, lapply, function(X) return(X$coefficients[2,1])))
+    p_slopes <- unlist(lapply(summary_models, lapply, function(X) return(X$coefficients[2,4])))
+    r_squares <- unlist(lapply(summary_models, lapply, function(X) return(X$adj.r.squared)))
+
+    ## Get the significance tokens
+    get.token <- function(p) {
+        if(p > 0.05) {
+            return("")
+        }
+        if(p < 0.05 && p > 0.01) {
+            return(".")
+        }
+        if(p < 0.01 && p > 0.001) {
+            return("*")
+        }
+        if(p < 0.001) {
+            return("**")
+        }
+    }
+
+    ## Summarise the results
+    results_out <- data.frame("Intercepts" = intercepts, "Int(p)" = p_intercepts, "Int(sig)" = sapply(p_intercepts, get.token),
+                              "Slope" = slopes, "Slo(p)" = p_slopes, "Slo(sig)" = sapply(p_slopes, get.token),
+                              "R^2 (adj)" = r_squares)
+
+
+    return(results_out)
+}
