7664284121563f62297883d7dea84c9b5b51b75a
TGuillerme <guillert@tcd.ie>
2019-01-27 17:46:07 +1000

    updated shells

--- Functions/shells/job.check.sh
+++ Functions/shells/job.check.sh
@@ -9,100 +9,103 @@ do
     ## Get the starting time
     time_entry=$(grep -n 'Entry time' ${job} | sed 's/:Entry time//g')
     let "time_entry += 1"
-    time_entry=$(sed -n ''"${time_entry}"'p' ${job})
-    time_entry=$(date -j -f '%a %b %d %T AEST %Y' "${time_entry}" "+%s")
+    time_entry=$(sed -n ''"${time_entry}"'p' ${job} | sed 's/AEDT//g' | sed 's/AEST//g')
+    time_entry=$(date -j -f '%a %b %d %T %Y' "${time_entry}" "+%s")
 
 
-    time_exit=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
-    if [ $time_exit -ne 0 ]
+    time_out=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
+    if [ $time_out -ne 0 ]
     then
         echo "JOB DONE"
         ## CPUT time
-        time_out=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
+        # time_out=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
         let "time_out += 1"
-        time_out=$(sed -n ''"${time_out}"'p' ${job})
-        time_out=$(date -j -f '%a %b %d %T AEST %Y' "${time_out}" "+%s")
+        time_out=$(sed -n ''"${time_out}"'p' ${job} | sed 's/AEDT//g' | sed 's/AEST//g')
+        time_out=$(date -j -f '%a %b %d %T %Y' "${time_out}" "+%s")
         time_run=$(echo $((time_out-time_entry)))
         echo "CPU time:"
         echo 'scale=5 ; '"${time_run}"'/3600*12' | bc
         echo ""
     else
-
-        ## Check if the job succeeded
-        runs_done=$(grep -c "Analysis completed" ${job})
-
-        ## Job done properly
-        if echo $runs_done | grep '4' > /dev/null
-        then
-            echo "JOB DONE"
-            ## CPUT time
-            time_out=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
-            let "time_out += 1"
-            time_out=$(sed -n ''"${time_out}"'p' ${job})
-            time_out=$(date -j -f '%a %b %d %T AEST %Y' "${time_out}" "+%s")
-            time_run=$(echo $((time_out-time_entry)))
-            echo "CPU time:"
-            echo 'scale=5 ; '"${time_run}"'/3600*12' | bc
-            echo ""
-        else
-
-            ## Get the number of success
-            success=$(grep "time out" ${job} | wc -l | sed 's/[[:space:]]//g')
-
-            ## Get the number of fails
-            # fails=$(grep "aborted" ${job} | wc -l | sed 's/[[:space:]]//g')
-            fails=$(echo "4-${success}" | bc)
-
-            ## Print the results
-            if [ $fails -ne 0 ]
-            then
-                echo "ABORTED JOBS"
-                aborted=$(grep "aborted" ${job} | head -1)
-                echo $aborted
-                ## Get the abortion date out
-                time_out=$(grep -n "${aborted}" ${job} | sed 's/:'"${aborted}"'//g')
-            else
-                echo "JOB-BIS SUCCESFUL"
-                grep "time out" ${job}
-
-                terminate=$(grep "rand time out" ${job})
-                if [ -n "$terminate" ]
-                then
-                    time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
-                else
-                    terminate=$(grep "mini time out" ${job})
-                    if [ -n "$terminate" ]
-                    then
-                        time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
-                    else
-                        terminate=$(grep "maxi time out" ${job})
-                        if [ -n "$terminate" ]
-                        then
-                            time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
-                        else
-                            terminate=$(grep "norm time out" ${job})
-                            if [ -n "$terminate" ]
-                            then
-                                time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
-                            fi
-                        fi
-                    fi
-                fi
-            fi
-
-            let "time_out += 1"
-            time_out=$(sed -n ''"${time_out}"'p' ${job})
-            time_out=$(date -j -f '%a %b %d %T AEST %Y' "${time_out}" "+%s")
-            time_run=$(echo $((time_out-time_entry)))
-            echo "CPU time:"
-            echo 'scale=5 ; '"${time_run}"'/3600*12' | bc #TG: 12 is the number of CPUs!
-            ## Check convergence
-            unconverge=$(grep 'MrBayes suspects that your runs have not converged because the tree' ${job} | wc -l | sed 's/[[:space:]]//g')
-            if [ $unconverge -ne 0 ]
-            then
-                echo "Warning: $unconverge runs did not converge!"
-            fi
-            echo ""
-        fi
+        echo "JOB ABORTED"
+        echo ""
     fi
+
+    #     ## Check if the job succeeded
+    #     runs_done=$(grep -c "Analysis completed" ${job})
+
+    #     ## Job done properly
+    #     if echo $runs_done | grep '4' > /dev/null
+    #     then
+    #         echo "JOB DONE"
+    #         ## CPUT time
+    #         time_out=$(grep -n 'Exit time' ${job} | sed 's/:Exit time//g')
+    #         let "time_out += 1"
+    #         time_entry=$(sed -n ''"${time_entry}"'p' ${job} | sed 's/AEDT//g' | sed 's/AEST//g')
+    #         time_entry=$(date -j -f '%a %b %d %T %Y' "${time_entry}" "+%s")
+    #         time_run=$(echo $((time_out-time_entry)))
+    #         echo "CPU time:"
+    #         echo 'scale=5 ; '"${time_run}"'/3600*12' | bc
+    #         echo ""
+    #     else
+
+    #         ## Get the number of success
+    #         success=$(grep "time out" ${job} | wc -l | sed 's/[[:space:]]//g')
+
+    #         ## Get the number of fails
+    #         # fails=$(grep "aborted" ${job} | wc -l | sed 's/[[:space:]]//g')
+    #         fails=$(echo "4-${success}" | bc)
+
+    #         ## Print the results
+    #         if [ $fails -ne 0 ]
+    #         then
+    #             echo "ABORTED JOBS"
+    #             aborted=$(grep "aborted" ${job} | head -1)
+    #             echo $aborted
+    #             ## Get the abortion date out
+    #             time_out=$(grep -n "${aborted}" ${job} | sed 's/:'"${aborted}"'//g')
+    #         else
+    #             echo "JOB-BIS SUCCESFUL"
+    #             grep "time out" ${job}
+
+    #             terminate=$(grep "rand time out" ${job})
+    #             if [ -n "$terminate" ]
+    #             then
+    #                 time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
+    #             else
+    #                 terminate=$(grep "mini time out" ${job})
+    #                 if [ -n "$terminate" ]
+    #                 then
+    #                     time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
+    #                 else
+    #                     terminate=$(grep "maxi time out" ${job})
+    #                     if [ -n "$terminate" ]
+    #                     then
+    #                         time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
+    #                     else
+    #                         terminate=$(grep "norm time out" ${job})
+    #                         if [ -n "$terminate" ]
+    #                         then
+    #                             time_out=$(grep -n "${terminate}" ${job} | sed 's/:'"${terminate}"'//g')
+    #                         fi
+    #                     fi
+    #                 fi
+    #             fi
+    #         fi
+
+    #         let "time_out += 1"
+    #         time_entry=$(sed -n ''"${time_entry}"'p' ${job} | sed 's/AEDT//g' | sed 's/AEST//g')
+    #         time_entry=$(date -j -f '%a %b %d %T %Y' "${time_entry}" "+%s")
+    #         time_run=$(echo $((time_out-time_entry)))
+    #         echo "CPU time:"
+    #         echo 'scale=5 ; '"${time_run}"'/3600*12' | bc #TG: 12 is the number of CPUs!
+    #         ## Check convergence
+    #         unconverge=$(grep 'MrBayes suspects that your runs have not converged because the tree' ${job} | wc -l | sed 's/[[:space:]]//g')
+    #         if [ $unconverge -ne 0 ]
+    #         then
+    #             echo "Warning: $unconverge runs did not converge!"
+    #         fi
+    #         echo ""
+    #     fi
+    # fi
 done
--- Functions/shells/make.con.tre.sh
+++ Functions/shells/make.con.tre.sh
@@ -6,8 +6,16 @@ tree=$2
 
 echo "${chain}_${tree}"
 
+## Isolate the files
+mkdir ${chain}_${tree}_tmp/
+mv ${chain}_${tree}.run*.t ${chain}_${tree}_tmp/
+
+## Get the right matrix
+MATRIXPATH="/Users/TGuillerme/Projects/CharactersCorrelation/Data/Simulations/Matrices"
+cp ${MATRIXPATH}/${chain}_${tree}.nex ${chain}_${tree}_tmp/
+
 ## Get in the right dir
-cd ${chain}/
+cd ${chain}_${tree}_tmp/
 
 ## Check if the chains are finished
 tail_end=$(tail -1 ${chain}_${tree}.run1.t)
@@ -36,30 +44,35 @@ fi
 if [ "$run1_finished" == "TRUE" ] && [ "$run2_finished" == "TRUE" ]
 then
 
-    list=$(ls ${chain}_${tree}*)
-    if echo $list | grep -q "${chain}_${tree}.mbcmd"
-    then
-        ## Create the <CHAIN>_<TREE> File
-        cp ${chain}_${tree}.mbcmd ${chain}_${tree}
-    fi
+    echo "Tree files ready for analysis."
 
-    ## Remove all the MrBayes commands and replace by sumt
-    mbcmd_start=$(grep -n "begin mrbayes;" ${chain}_${tree} | sed 's/:begin mrbayes;//g')
-    sed ''"$mbcmd_start"',$d' ${chain}_${tree} > tmp
-    cp ${chain}_${tree} ${chain}_${tree}.bkp
-    mv tmp ${chain}_${tree}
-    echo "begin mrbayes;" >> ${chain}_${tree}
-    echo "set autoclose=yes nowarn=yes;" >> ${chain}_${tree}
-    echo "sumt;" >> ${chain}_${tree}
-    echo "end;" >> ${chain}_${tree}
+    # list=$(ls ${chain}_${tree}*)
+    # if echo $list | grep -q "${chain}_${tree}.mbcmd"
+    # then
+    #     ## Create the <CHAIN>_<TREE> File
+    #     cp ${chain}_${tree}.mbcmd ${chain}_${tree}
+    # fi
 
-    ## Run the script
-    mb ${chain}_${tree}
+    # ## Remove all the MrBayes commands and replace by sumt
+    # mbcmd_start=$(grep -n "begin mrbayes;" ${chain}_${tree} | sed 's/:begin mrbayes;//g')
+    # sed ''"$mbcmd_start"',$d' ${chain}_${tree} > tmp
+    # cp ${chain}_${tree} ${chain}_${tree}.bkp
+    # mv tmp ${chain}_${tree}
+    # echo "begin mrbayes;" >> ${chain}_${tree}
+    # echo "set autoclose=yes nowarn=yes;" >> ${chain}_${tree}
+    # echo "sumt;" >> ${chain}_${tree}
+    # echo "end;" >> ${chain}_${tree}
 
-    ## Return to parent dir
-    cd ..
+    # ## Run the script
+    # mb ${chain}_${tree}
+
+    # ## Return to parent dir
+    # cd ..
 
 else
+
+    echo "Getting tree files ready for analysis:"
+
     ## Count the number of lines missing in run1
     if [ "$run1_finished" == "TRUE" ]
     then
@@ -103,31 +116,38 @@ else
     then
         echo "end;" >> ${chain}_${tree}.run1.t
     fi
+    
     if [ "$run2_finished" == "FALSE" ]
     then
         echo "end;" >> ${chain}_${tree}.run2.t
     fi
 
-    ## Remove all the MrBayes commands and replace by sumt
-    mbcmd_start=$(grep -n "begin mrbayes;" ${chain}_${tree} | sed 's/:begin mrbayes;//g')
-    sed ''"$mbcmd_start"',$d' ${chain}_${tree} > tmp
-    cp ${chain}_${tree} ${chain}_${tree}.bkp
-    mv tmp ${chain}_${tree}
-    echo "begin mrbayes;" >> ${chain}_${tree}
-    echo "set autoclose=yes nowarn=yes;" >> ${chain}_${tree}
-    echo "sumt;" >> ${chain}_${tree}
-    echo "end;" >> ${chain}_${tree}
+fi
 
-    ## Run the script
-    mb ${chain}_${tree}
+echo "Setting up mb analysis:"
 
-    ## Remove the backup (if consensus tree exists)
-    if ls | grep -q ${chain}_${tree}.con.tre
-    then
-        rm ${chain}_${tree}*.bkp
-    fi
+## Remove all the MrBayes commands and replace by sumt
+# mbcmd_start=$(grep -n "begin mrbayes;" ${chain}_${tree} | sed 's/:begin mrbayes;//g')
+# sed ''"$mbcmd_start"',$d' ${chain}_${tree} > tmp
+# cp ${chain}_${tree} ${chain}_${tree}.bkp
+mv ${chain}_${tree}.nex ${chain}_${tree}
+echo "begin mrbayes;" >> ${chain}_${tree}
+echo "set autoclose=yes nowarn=yes;" >> ${chain}_${tree}
+echo "sumt;" >> ${chain}_${tree}
+echo "end;" >> ${chain}_${tree}
 
-    ## Return to parent dir
+## Run the script
+mb ${chain}_${tree}
+
+## Remove the backup (if consensus tree exists)
+if ls | grep -q ${chain}_${tree}.con.tre
+then
+    ## Move the relevant items to the previous folder
+    mv ${chain}_${tree}.con.tre ../
+    mv ${chain}_${tree}.run1.t ../
+    mv ${chain}_${tree}.run2.t ../
     cd ..
-    
+    rm -R ${chain}_${tree}_tmp/
+else 
+    cd ..
 fi
